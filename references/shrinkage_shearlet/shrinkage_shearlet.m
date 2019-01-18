a2677a7e3faa2fe7792ca26adbf55fcee480f196
Forsworns <378974295@qq.com>
2019-01-18 13:20:34 +0800

    update

--- references/shrinkage_shearlet/shrinkage_shearlet.m
+++ references/shrinkage_shearlet/shrinkage_shearlet.m
@@ -9,29 +9,32 @@
 
 % for Kobe
 load("../../dataset/kobe32_cacti.mat")
-img = orig(:,:,1);
-mask = mask(:,:,1);
+img = orig(:,:,1:8);
+mask = mask(:,:,1:8);
 imgMasked = mask.*img;
 stopFactor = 0.005; % 迭代到什么精细程度
 iteration = 100;
 nor = max(img(:)) - min(img(:));
 
-imgInpainted = 0;
-coeffsNormalized = SLnormalizeCoefficients2D(SLsheardec2D(imgMasked,shearletSystem),shearletSystem);
-delta = max(abs(coeffsNormalized(:)));
-lambda = (stopFactor)^(1/(iteration-1));
-for i=1:iteration
-    res = mask.*(imgMasked-imgInpainted);
-    coeffs = SLsheardec2D(imgInpainted+res,shearletSystem);
-    coeffs = coeffs.*(abs(SLnormalizeCoefficients2D(coeffs,shearletSystem))>delta);            
-    imgInpainted = SLshearrec2D(coeffs,shearletSystem);
-    figure(2);
-    colormap(gray);
-    imagesc(imgInpainted);title(['iteration : ' num2str(i, '%d'),'/',num2str(iteration,'%d')]);
-    drawnow();
-    delta=delta*lambda;
-    disp(i);
-end  
+imgInpainted = zeros(size(img));
+for j =1:8
+    coeffsNormalized = SLnormalizeCoefficients2D(SLsheardec2D(imgMasked(:,:,j),shearletSystem),shearletSystem);
+    delta = max(abs(coeffsNormalized(:)));
+    lambda = (stopFactor)^(1/(iteration-1));
+    for i=1:iteration
+        res = mask(:,:,j).*(imgMasked(:,:,j)-imgInpainted(:,:,j));
+        coeffs = SLsheardec2D(imgInpainted(:,:,j)+res,shearletSystem);
+        coeffs = coeffs.*(abs(SLnormalizeCoefficients2D(coeffs,shearletSystem))>delta);            
+        imgInpainted(:,:,j) = SLshearrec2D(coeffs,shearletSystem);
+        figure(2);
+        colormap(gray);
+        imagesc(imgInpainted(:,:,j));title(['iteration : ' num2str(i, '%d'),'/',num2str(iteration,'%d')]);
+        drawnow();
+        delta=delta*lambda;
+        disp(i);
+    end
+end
+
 
 psnr_img = psnr(img/nor,imgInpainted/nor);
 ssim_img = ssim(img/nor,imgInpainted/nor);
