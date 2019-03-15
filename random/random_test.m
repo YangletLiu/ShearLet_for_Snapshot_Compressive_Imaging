283ab0766116d4cabf1de759e3968148690b23dd
Forsworns <378974295@qq.com>
2019-03-15 23:50:35 +0800

    整理

--- random/random_test.m
+++ random/random_test.m
@@ -5,7 +5,7 @@ home;
 
 bFig = true;
 bTest = false;
-%% DATASET
+%% Initialize
 load("kobe32_cacti.mat") % orig,meas,mask（原始图像，压缩图像，压缩时用的mask
 test_data = 1; % meas帧数
 
@@ -62,44 +62,8 @@ for k = test_data
     x_rp	= random_projection_without_optimization(L,s,n,niter,M,captured,x,bTest);
     time = toc;
     % x_rp = TV_denoising(x_rp/255,0.05,10)*255;
-%% 将图片归一
-    nor         = 255;
-    ratio  = max(max(x))/nor; % 原图中该块相对255的系数
-    min_rp = min(min(x_rp));
-    max_rp = max(max(x_rp));
-    nor_rp = max_rp-min_rp;
-    for f=1:codedNum
-        x_rp(:,:,f) = (x_rp(:,:,f)-min_rp(f))/nor_rp(f)*ratio(f);
-    end
-    psnr_x_rp = zeros(codedNum,1);
-    ssim_x_rp = zeros(codedNum,1);
-%% DISPLAY
-    figure(1); 
-    for i=1:codedNum
-        if bOrig
-            colormap gray;
-            subplot(121);   
-            imagesc(x(:,:,i));
-            set(gca,'xtick',[],'ytick',[]);
-            title('orig');
-
-            subplot(122);   
-            imagesc(x_rp(:,:,i));  	
-            set(gca,'xtick',[],'ytick',[]); 
-
-            psnr_x_rp(i) = psnr(x_rp(:,:,i), x(:,:,i)./nor);
-            ssim_x_rp(i) = ssim(x_rp(:,:,i), x(:,:,i)./nor);
-            title({['frame : ' num2str(i, '%d')], ['PSNR : ' num2str(psnr_x_rp(i), '%.4f')], ['SSIM : ' num2str(ssim_x_rp(i), '%.4f')]});
-        else 
-            colormap gray;
-            imagesc(x_rp(:,:,i));  	
-            set(gca,'xtick',[],'ytick',[]); 
-            title(['frame : ' num2str(i, '%d')]);
-        end
-        pause(1);
-    end
-    psnr_rp = mean(psnr_x_rp);
-    ssim_rp = mean(ssim_x_rp);
+%% Display
+    my_display(x,x_rp,codedNum,bOrig);
 end
 
 % 直接观测投影出的矩阵之间的差距
