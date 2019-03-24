43ebeacc845f3ba1ecf4467c2b0210e5910ce394
Forsworns <378974295@qq.com>
2019-03-24 15:43:13 +0800

    update

--- random/test.m
+++ random/test.m
@@ -18,7 +18,7 @@ for i=2:NN
     end
 end
 
-
+s = 2;
 L2 = 100; 
 cal_num=3; % 估计前几个系数
 estimated_theta = zeros(cal_num,1);
@@ -28,24 +28,37 @@ for i =1:L2
     for j=1:L1
         order = randperm(N); 
         % 这里取正负是考虑到由mask中的元素确定的正负是确定的，否则L行中某列上只要非零就都相同
-        ps = order(1:N/4);
-        ns = order(1+N/4:N/2);
+        ps = order(1:N/2/s);
+        ns = order(1+N/2/s:N/s);
 
-        Phi(j,:) = Phi(j,:) + to_vec(ps,N,M,f);
-        Phi(j,:) = Phi(j,:) - to_vec(ns,N,M,f);
+        Phi(j,:) = Phi(j,:) + extract_M(ps,N,M,f);
+        Phi(j,:) = Phi(j,:) - extract_M(ns,N,M,f);
     end
-
+    
+%     Phi = zeros(L1,NN);
+%     order = randperm(N*f*L1);
+%     nonzero_num = N*f*L1/s;
+%     positive = order(1:nonzero_num/2);
+%     negtive = order(nonzero_num/2+1:nonzero_num);
+%     Phi(positive) = 1;
+%     Phi(negtive) = -1; 
+    
+    Phi = sqrt(s)*Phi;
+    
+    mean(Phi(:)) % 均值
+    sum(Phi(:).*Phi(:))/(L1*N*f) % 方差
+    
     u = Phi*x;
     v = Phi*dft(:,1:cal_num); % 对称，行列一样
     
     % 之后换中位数
-    estimated_theta = estimated_theta + (u.'*v).';
+    estimated_theta = estimated_theta + (u.'*v).'/L1;
 end
 
 estimated_theta = estimated_theta/L2;
 real_theta = fft(x);
 
-function vec = to_vec(idx,N,M,f)
+function vec = extract_M(idx,N,M,f)
     vec = zeros(1,N*f);
     for i =1:f
         vec(N*(i-1)+idx) = M(N*(i-1)+idx);
