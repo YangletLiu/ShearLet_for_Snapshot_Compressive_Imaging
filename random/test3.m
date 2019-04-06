d3c8559d3e9481e7eb00760debd5d9f5e50798b5
Forsworns <378974295@qq.com>
2019-04-06 16:27:40 +0800

    考虑相关性？

--- random/test3.m
+++ random/test3.m
@@ -40,39 +40,38 @@ end
 
 L2 = 10; 
 
-otherFrames = rand([1,N,f]);
-f1 = M(1:256);
-f2 = M(257:512);
-f12 = f1.*f2;
-
+if f==2
+    f1 = M(1:256);
+    f2 = M(257:512);
+    f12 = f1.*f2;
+end
+% estimated_thetaz至少前百分之十的系数需要估计准确（最后的高频信息不重要），
+% 同时第一个系数是平均值，不准确也可以，恢复出的图像只是有一个比例的误差
 estimated_theta = zeros(1,cal_num);
 for ite =1:L2
     L1 = 1e4;
     Phi = zeros(L1,NN);
-    y = zeros(L1,1);
-    for j=1:L1
-        order = randperm(N); 
-        % 这里取正负是考虑到由mask中的元素确定的正负是确定的，否则L行中某列上只要非零就都相同
-        ps = order(1:N/2);
-        ns = order(1+N/2:N);
-        Phi(j,:) = Phi(j,:) + extract_M(ps,N,M,f);
-        Phi(j,:) = Phi(j,:) - extract_M(ns,N,M,f);
-    end
-       
-%     order = randperm(N*f*L1);
-%     nonzero_num = N*f*L1;
-%     positive = order(1:nonzero_num/40*21);
-%     negtive = order(nonzero_num/40*21+1:nonzero_num);
-%     Phi(positive) = 1;
-%     Phi(negtive) = -1;
-%     
-%     otherFrames(otherFrames<0.5) = -1;
-%     otherFrames(otherFrames>0.5) = 1;
-%     for i=1:L1
-%         for k=2:f
-%             Phi(i,(k-1)*N+1:k*N) = Phi(i,1:N).*otherFrames(:,:,k);
-%         end
+    
+%     for j=1:L1
+%         order = randperm(N); 
+%         % 这里取正负是考虑到由mask中的元素确定的正负是确定的，否则L行中某列上只要非零就都相同
+%         ps = order(1:N/2);
+%         ns = order(1+N/2:N);
+%         Phi(j,:) = Phi(j,:) + extract_M(ps,N,M,f);
+%         Phi(j,:) = Phi(j,:) - extract_M(ns,N,M,f);
 %     end
+       
+    % 另一个写法
+    order = randperm(NN*L1);
+    positive = order(1:NN*L1/40*21);
+    negtive = order(NN*L1/40*21+1:NN*L1);
+    Phi(positive) = 1;
+    Phi(negtive) = -1;
+    for i=1:L1
+        for k=2:f
+            Phi(i,(k-1)*N+1:k*N) = Phi(i,1:N).*M((k-1)*N+1:k*N)';
+        end
+    end
     
     means = mean(Phi(:)) % 均值
     var = sum((Phi(:)-means).*(Phi(:)-means))/(L1*N*f) % 方差
@@ -80,14 +79,17 @@ for ite =1:L2
     u = Phi*x;
     v = Phi*dft; % 对称，行列一样
     
-    bias = 0;
-    for j=1:256
-        bias = bias + f12(j)*u(j)*v(j+256,:)+f12(j)*u(j+256)*v(j,:);   
+    estimated_theta = estimated_theta + (u'*v)/L1;
+    
+    if f==2
+        bias = 0;
+        for j=1:256
+            bias = bias + f12(j)*x(j)*dft(j+256,:)+f12(j)*x(j+256)*dft(j,:);   
+        end
+        estimated_theta = estimated_theta + bias;
     end
     
-    estimated_theta = estimated_theta + (u'*v-bias)/L1;
 end
-
 estimated_theta = estimated_theta/L2;
 
 real_theta = fft(x);
@@ -95,8 +97,7 @@ estimated_theta = estimated_theta.';
 error = norm(estimated_theta-real_theta);
 
 estimated_x = real(ifft(estimated_theta));
-estimated_x = reshape(estimated_x,[n,n,f]);
-my_display(reshape(x,[n,n,f]),estimated_x,f,true)
+my_display(reshape(x,[n,n,f]),reshape(estimated_x,[n,n,f]),f,true)
 
 function vec = extract_M(idx,N,M,f)
     vec = zeros(1,N*f);
